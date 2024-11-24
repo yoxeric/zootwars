44804a5c1c98b4d8953be0d6c709aeb3f2e2a067
yoxeric <82884760+yoxeric@users.noreply.github.com>
2024-11-25 00:53:48 +0100

    Add files via upload

--- streetbond/scripts/player.gd
+++ streetbond/scripts/player.gd
@@ -1,22 +1,14 @@
 extends CharacterBody3D
 
-
 const SPEED = 100
-const JUMP_VELOCITY = 4.5
 
+@onready var camera = $Camera3D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var cam_dir := Input.get_vector("front", "back", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	camera.position = camera.position + Vector3(0, cam_dir.x, cam_dir.x)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
--- streetbond/scripts/warrior.gd
+++ streetbond/scripts/warrior.gd
@@ -1,102 +1,113 @@
 extends CharacterBody3D
 
 
-const SPEED = 20
 const JUMP_VELOCITY = 4.5
 
-var Health = 100
+@export var team = 0
+@export var health = 4
+@export var damage = 2
+@export var speed = 20
+@export var attack_speed = 2
+@export var price = 4
 var state = 0
 var direction
-var target
-var enemy
+var targets = [ ]
+var enemys = [ ]
 
-@onready var base_2: Node3D = $"../base2"
 @onready var base_1: Node3D = $"../base1"
-
-@export var is_enemy = 0
+@onready var base_2: Node3D = $"../base2"
 
 @onready var health_bar: ProgressBar = $Sprite3D/SubViewport/ProgressBar
 @onready var timer: Timer = $Timer
+@onready var animation_player = $AnimationPlayer
 
-func _ready() -> void:
-	if is_enemy == 0:
-		target = base_2
-	if is_enemy == 1:
-		target = base_1
+func _ready():
+	health_bar.max_value = health
+	if team == 1:
+		targets.append(base_2)
+	if team == 2:
+		targets.append(base_1)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if not is_instance_valid(target):
-		if is_enemy == 0:
-			target = base_2
-		if is_enemy == 1:
-			target = base_1
-		
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#states
+	# 1 -> run
+	# 2 -> shase
+	# 3 -> attack
 	if state == 0 || state == 1:
-		direction = (target.position - position).normalized()
-		look_at(target.position)
+		var trgt = targets.back()
+		if is_instance_valid(trgt):
+			direction = (trgt.position - position).normalized()
+			look_at(trgt.position)
 	if state == 2:
 		direction = 0
+		if enemys.is_empty():
+			state = 0
+			timer.stop()
 		
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 	
-	if Health <= 0:
+	if health <= 0:
+		var win = price / 2 if price / 2 > 1 else 1
+		if team == 1:
+			base_2.money += win
+		if team == 2:
+			base_1.money += win
 		queue_free()
+
+func take_dmg(dmg):
+	health -= dmg
+	if health_bar.value < dmg:
+		dmg = health_bar.value
+	health_bar.value = health
+	if health >= 0:
+		return false
+	else:
+		return true
+
+func _on_timer_timeout() -> void:
+	if not enemys.is_empty():
+		var enemy = enemys.back()
+		var is_dead = enemy.take_dmg(damage)
+		if is_dead:
+			targets.erase(enemy)
+			enemys.erase(enemy)
+	timer.start(attack_speed)
+	
 
 
 func _on_view_body_entered(body: Node3D) -> void:
 	if (state == 0):
-		var pos = (body.position - position).normalized()
-		
-		if position.distance_to(pos) > 5 && is_enemy == 0 && body.is_enemy == 1:
-			target = body
+		#var pos = (body.position - position).normalized()
+		#if position.distance_to(pos) > 5 && team != body.team:
+		if  body.team && team != body.team:
+			targets.append(body)
 			state = 1
-		if position.distance_to(pos) > 5 && is_enemy == 1 && body.is_enemy == 0:
-			target = body
-			state = 1
-	
-
-func take_dmg(dmg):
-	#if health_bar.value < dmg:
-		#dmg = health_bar.value
-	Health -= dmg
-	health_bar.value = Health
 	
 
 func _on_view_body_exited(body: Node3D) -> void:
-	#state = 0
-	pass
+	if targets.has(body) && body != base_1 && body != base_2:
+		targets.erase(body)
 
 
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	#body.take_dmg(50)
-	enemy = body
-	timer.start(1)
-	state = 2
-
+	if (body.team != team):
+		enemys.append(body)
+		timer.start(attack_speed)
+		print("attack")
+		state = 2
 
 func _on_hitbox_body_exited(body: Node3D) -> void:
-	enemy = null
-	timer.stop()
-	state = 0
-
-func _on_timer_timeout() -> void:
-	if enemy != null:
-		enemy.take_dmg(50)
+	if (enemys.has(body)):
+		enemys.erase(body)
